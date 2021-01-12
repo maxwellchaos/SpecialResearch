28ca0f1c02e6491ebf7d2bdad9d4780da9fba9cd
Максим Холуев <maxwellchaos@gmail.com>
2021-01-12 23:48:19 +0300

    initial

--- SpecialResearch/Data/SpecialResearchContext.cs
+++ SpecialResearch/Data/SpecialResearchContext.cs
@@ -43,6 +43,52 @@ namespace SpecialResearch.Data
                 StageName = "Заявка закрыта"
             });
 
+
+
+            ////Права доступа(роли) по умолчанию
+            //builder.Entity<IdentityRole>().HasData(new IdentityRole
+            //{
+            //    Id = "a1c31d1c-5cc3-4aee-89eb-ecdf62d5a8a1",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    Name = "admin",
+            //    NormalizedName = "ADMIN"
+            //});
+            //builder.Entity<IdentityRole>().HasData(new IdentityRole
+            //{
+            //    Id = "ea5f39f9-3bcd-42bf-b7cb-a3b2dfbf317d",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    Name = "receiver",
+            //    NormalizedName = "RECEIVER"
+            //});
+            //builder.Entity<IdentityRole>().HasData(new IdentityRole
+            //{
+            //    Id = "c03b6828-8288-41fb-b295-3f410aa4b3c0",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    Name = "tester",
+            //    NormalizedName = "TESTER"
+            //});
+            //builder.Entity<IdentityRole>().HasData(new IdentityRole
+            //{
+            //    Id = "f51eee01-1b9d-490b-b630-ae5997d4c65b",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    Name = "controller",
+            //    NormalizedName = "CONTROLLER"
+            //});
+            //builder.Entity<IdentityRole>().HasData(new IdentityRole
+            //{
+            //    Id = "78ff2dc9-f218-4c8e-921c-54977ac86bf4",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    Name = "manager",
+            //    NormalizedName = "MANAGER"
+            //});
+
+            ////Юзер
+            //builder.Entity<IdentityUser>().HasData(new IdentityUser
+            //{
+            //    Id = "b9724707-432b-4ea1-a6ee-4ff3e61988c8",//Сгенерирован на сайте https://www.guidgenerator.com/online-guid-generator.aspx
+            //    UserName = "admin",
+            //    NormalizedUserName = "admin".ToUpper(),
+            //    Email = "mt@mail.com",
+            //    NormalizedEmail = "mt@mail.com".ToUpper(),
+            //    EmailConfirmed = true,//чтобы было
+            //    PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "admin"),
+            //    SecurityStamp = string.Empty
+            //});
         }
     }
 }
